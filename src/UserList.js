69b9d4f6aad74b07459e3abd6f3adc8a897e6fb9
rk <grenkiat@gmail.com>
2024-03-08 11:06:20 +0800

    first commit

---
+++ src/UserList.js
@@ -0,0 +1,34 @@
+// src/UserList.js
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Fetch users from the Spring Boot backend
+    axios
+      .get("http://localhost:8080/api/users")
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
